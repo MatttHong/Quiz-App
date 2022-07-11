52a952f4ece453f232711a553a5a788d5d6a01d1
MatttHong <matthew.y.hong@vanderbilt.edu>
2022-07-11 18:03:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,16 +3,17 @@ const answer = Array.from(document.querySelectorAll(".answer"))
 const questions = document.querySelectorAll(".question-item")
 const alert = document.getElementById("alert")
 
+//create event listener to listen to when answers are submitted
 form.addEventListener("submit", e => {
   e.preventDefault()
-
+  //make unanswered questions red/wrong
   questions.forEach(questionItem => {
     questionItem.classList.remove("correct")
     questionItem.classList.add("incorrect")
   })
-
+  //score questions
   score()
-
+  //refresh page after submission
   setTimeout(() => {
     window.location.reload()
   }, 3000)
